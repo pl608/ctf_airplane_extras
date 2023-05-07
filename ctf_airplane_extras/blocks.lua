21bf0bfb8205f569f5af4d1c66ddd90a9cc14169
pl608 <computerprojects46@gmail.com>
2023-05-07 12:53:16 -0700

    accidental revert
    attempting to switch back to the last commit

--- ctf_airplane_extras/blocks.lua
+++ ctf_airplane_extras/blocks.lua
@@ -1,24 +1,15 @@
 
 --ctf_airplane_extras nodes:
 -- Note: only blue planes atm
-function extras.register_spawnbloc(color)
-    minetest.register_node("ctf_airplane_extras:airplane_spawnblock_"..color,{
-        description = "Airplane Spawn Block ("..color..")",
-        --inventory_image = "default_stone_brick.png^pa28.png",
-        tiles = {"default_stone_brick.png^pa28.png"},
-        groups = {immortal = 1, not_in_creative_inventory=1},
-        on_punch = function(pos, node, puncher)
-            internal.spawn_plane(pos, node, puncher, color)
-            minetest.chat_send_player(puncher:get_player_name(), 'Spawned you an airplane!')
-        end,
-    })
-end
-extras.register_spawnbloc('red')
-extras.register_spawnbloc('orange')
-extras.register_spawnbloc('blue')
-extras.register_spawnbloc('purple')
-extras.register_spawnbloc('green')
---[[ Only need one type...
+minetest.register_node("ctf_airplane_extras:airplane_spawnblock_red",{
+    description = "Airplane Spawn Block",
+    --inventory_image = "default_stone_brick.png^pa28.png",
+    tiles = {"default_stone_brick.png^pa28.png"},
+    groups = {immortal = 1, not_in_creative_inventory=1},
+    on_punch = function(pos, node, puncher)
+        internal.spawn_plane(pos, node, puncher, true)
+    end,
+})--[[ Only need one type...
 minetest.register_node("ctf_airplane_extras:airplane_spawnblock_blue",{
     description = "Airplane Spawn Block",
     --inventory_image = "default_stone_brick.png^pa28_blue.png",
@@ -30,7 +21,7 @@ minetest.register_node("ctf_airplane_extras:airplane_spawnblock_blue",{
 })]]
 
 -- will spawn a plane twice on the same node, nothing done because only solutions are to track every airplane or have each node only spawn one plane
---[[[
+
 minetest.register_abm({
 	nodenames = {"ctf_airplane_extras:airplane_spawnblock_red"},
 	--neighbors = {"default:water_source", "default:water_flowing"},
@@ -39,7 +30,7 @@ minetest.register_abm({
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		internal.spawn_plane(pos, node,nil, true)
 	end
-})]]
+})
 --[[see line 12
 minetest.register_abm({
 	nodenames = {"ctf_airplane_extras:airplane_spawnblock_blue"},
