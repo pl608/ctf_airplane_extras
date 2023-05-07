52e5d6069888957840af1e6f1afa2f362224f656
pl608 <computerprojects46@gmail.com>
2023-05-07 09:53:27 -0700

    commit
    updating to make it less laggy(idk how successful it is. Doesnt spawn planes rn working on that...

--- ctf_airplane_extras/init.lua
+++ ctf_airplane_extras/init.lua
@@ -28,15 +28,21 @@ bomb_override = {   "ctf_ranged:pistol",
                     "ctf_ranged:sniper_magnum",
                     "ctf_airplane_extras:gasoline"
 } 
-airplanes_destroyed_red = 0
-airplanes_destroyed_blue = 0
+internal.airplanes_destroyed = {}
+
 airutils.fuel = {["ctf_airplane_extras:gasoline"] = 15/2} -- just kicked biofuel off the market :P
 bomb_dejitter_time = 1--drop rate in secs higher the less bombs dropped per sec ie 10 = .1 dps 1 = 1 dps .1 = 1 dps(only intergers allowed :) and 0 means you die :P
-last_drop = 0
+last_drop = 0 -- need to move this to player meta so that you can spawn bombs faster. for lag reasons not messing with it now
 
 local zero = {x=0,y=0,z=0}
 --pa28/utilites.lua:33
-function extras.paint_team(self,player)
+function extras.paint_team(self,player,color)
+    if color==nil then 
+        if player == nil then
+            return
+        end
+        color = ctf_teams.get(player)
+    end
     local texture_name = "pa28_painting.png"
     self.team = ctf_teams.get(player)
     minetest.log(tostring(self.team))
@@ -46,7 +52,7 @@ function extras.paint_team(self,player)
         for _, texture in ipairs(l_textures) do
             local indx = texture:find(texture_name)
             if indx then
-                l_textures[_] = texture_name.."^[multiply:".. internal.colors[ctf_teams.get(player)]
+                l_textures[_] = texture_name.."^[multiply:".. internal.colors[color]
             end
         end
 	    self.object:set_properties({textures=l_textures})
@@ -55,12 +61,9 @@ end
 
 --pa28/utilities.lua:234,
 function extras.airplane_destroy(color)
-    if color == "red" then
-        airplanes_destroyed_red = airplanes_destroyed_red+1
+    if internal.airplanes_destroyed[color] then internal.airplanes_destroyed[color] = 1
     end
-    if color == "blue" then
-        airplanes_destroyed_blue = airplanes_destroyed_blue+1
-    end
+    internal.airplanes_destroyed[color] = internal.airplanes_destroyed[color] +1
 end
 --pa28/entites.lua:366
 function extras.DropBomb(self, player)
@@ -80,7 +83,7 @@ function extras.DropBomb(self, player)
             if inv:contains_item("main", inventory_item) then
                 local stack = ItemStack(inventory_item .. " 1")
                 inv:remove_item("main", stack)
-                if ctf_teams.get(player) == color then
+                if ctf_teams.get(player) == color then -- kinda reduntant but ig its an extra check
                     minetest.add_entity(player:get_pos(), "ctf_airplane_extras:missile_" .. color)
                 else
                     minetest.add_entity(player:get_pos(), "ctf_airplane_extras:missile_blue")
@@ -90,30 +93,24 @@ function extras.DropBomb(self, player)
             end
         end
     end
-    drop("red")
-    drop("orange")
-    drop("purple")
-    drop("blue")
+    --drop("red")
+    --drop("orange")
+    --drop("purple")
+    --drop("blue")
+    drop(team)
 end
 
-function internal.spawn_plane(pos, node, other, is_red)
+function internal.spawn_plane(pos, node, other, color)
     --if airplanes_destroyed_red <= 0 then return end
     if other ~= nil then if other:get_wielded_item():get_name() or "did not work" == "ctf_map:adminpick" then return end end
     --if plane_spawned[pos_tostring(pos)] then return else plane_spawned[pos_tostring(pos)] = true end
-    if is_red then 
-        if airplanes_destroyed_red <= 0 then
-            return 
-        else 
-            airplanes_destroyed_red = airplanes_destroyed_red-1 
-        end
+    
+    if internal.airplanes_destroyed[color] <= 0 then
+        return 
+    else 
+        internal.airplanes_destroyed[color] = internal.airplanes_destroyed[color]-1 
     end
-    if not is_red then 
-        if airplanes_destroyed_blue <= 0 then 
-            return 
-        else 
-            airplanes_destroyed_blue = airplanes_destroyed_blue-1 
-        end
-    end
+    
     
     local pointed_pos = pos
     --local node_below = minetest.get_node(pointed_pos).name
@@ -122,6 +119,7 @@ function internal.spawn_plane(pos, node, other, is_red)
     pointed_pos.y=pointed_pos.y+2.5
     --plane_spawned[pos_tostring(pos)] = true
     local pa28_ent = minetest.add_entity(pointed_pos, "pa28_custom:pa28")
+    extras.paint_team(pa28_ent, nil, color) -- should color plane on spawn 
     return itemstack
 end
 
@@ -298,7 +296,17 @@ function get_table_len(table)
 end
 
 -- DEBUG TOOLS
---[[
+----[[
+minetest.register_chatcommand('set_spawn', {
+    params = "<color> <amount>",
+	description = "set amount of planes in <color> to 10",
+	privs = {interact = true},
+    func = function(name, param)
+        
+        local pname, score = string.match(param, "^(.*) (.*)$")
+        internal.airplanes_destroyed[pname] = 10
+    end
+})
 minetest.register_chatcommand("vars", {
 	params = "",
 	description = "shows vars",
@@ -320,8 +328,8 @@ minetest.register_chatcommand("drop", {
         extras.DropBomb(player)
     end
 })
-
-]]
+--
+----]]
 dofile(minetest.get_modpath("ctf_airplane_extras") .. "/blocks.lua")
 dofile(minetest.get_modpath("ctf_airplane_extras") .. "/items.lua")
 dofile(minetest.get_modpath("ctf_airplane_extras") .. "/entities.lua")
